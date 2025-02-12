bf5f7f258f48b95ba5a1eee3a3477835f6c79df6
Yukihyuo <danielperezglz123@gmail.com>
2025-02-11 23:44:09 -0600

    login terminado

--- Web/src/app/Login.tsx
+++ Web/src/app/Login.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
             </div>
             <div className="relative hidden bg-muted lg:block">
                 <img
-                    src="/placeholder.svg"
+                    src="/bg.webp"
                     alt="Image"
                     className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
                 />
